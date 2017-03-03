9f3e643d5837280e16fbb401fe1b850ea47dee20
p89 <pawel.prochal@gmail.com>
2017-03-03 11:25:23 +0100

    2nd api integration + error checks

--- main.js
+++ main.js
@@ -22,84 +22,11 @@ $(document).ready(function() {
                 $(target).addClass('pm251');
                 break;    
             }
-     }
+     };
 
-    const apiUrl = 'http://powietrze.malopolska.pl/_powietrzeapi/api/dane?act=danemiasta&ci_id=01';
-    const prefix = 'https://cors-anywhere.herokuapp.com/';
-    const finalUrl = prefix + apiUrl;
+     function useBackupAPI () {
 
-    // check if ajax retrieved valid data:
-
-    // $.getJSON(finalUrl, function(results) {
-    //     if (results.dane.actual[0].details[0].g_nazwa) {
-    // console.log(results.dane.actual[0].details[0].g_nazwa); } else { console.log('nie dziala') }
-    // ;
-    // });    
-    
-    
-    // --------- code for powietrze.malopolska.pl api
-    
-
-    $.getJSON(finalUrl, function(results) {
-
-    // there are several ways the getJSON api download can fail:
-    // a) proxy can die  b) api can die  c) api and proxy can live but api can return null
-    // & possibly more
-    // added additional condition to check whether the random index returns true 
-    
-    if (results.dane.actual[0].details[0]) {    
-
-    
-        
-        var data = results.dane.actual;
-        //console.log(data[2].station_name);
-    // object constructor for each field in which we're going to present the data
-
-        function StatusObject (id) {
-
-            this.fieldName = `#div${id}`;
-            this.name = data[id].station_name;
-            this.hour = data[id].station_hour + ":00";
-            this.status = data[id].details[0].g_nazwa;
-            this.pm10 = data[id].details[0].o_value;
-            this.pm25 = data[id].details[1].o_value;
-            this.no2 = data[id].details[2].o_value;
-            this.co = data[id].details[3].o_value;
-    
-            this.getValues = function () {
-                return {
-                    "Nazwa stacji:"                 : this.name,
-                    "Godzina pomiaru:"              : this.hour,
-                    "Aktualna jakość powietrza:"    : this.status,
-                    "Poziom PM 10:"                 : this.pm10,
-                    "Poziom PM 2,5:"                : this.pm25,
-                    "Poziom NO2:"                   : this.no2,
-                    "Poziom CO:"                    : this.co
-                };
-
-            };
-        };
-
-        let Station0 = new StatusObject(0);
-        // let Station1 = new StatusObject(1);
-        // let Station2 = new StatusObject(2);
-        // let Station3 = new StatusObject(3);
-        // let Station4 = new StatusObject(4);
-        // let Station5 = new StatusObject(5);
-
-        console.log(Station0.getValues());
-
-    
-  
-    
-
-    } else {
-      console.log('Error, cannot retrieve data.');
-    };
-
-}).fail(function() {  
-
-       // --------- code for api.waqi.info 
+        // --------- code for api.waqi.info 
 
     //alt token for Waqi bb892cfadf429d1f082713c403d0409f853d9559
     const apiWaqi = "https://api.waqi.info/map/bounds/?latlng=49.906700,20.172406,50.135416, 19.753533&token=116aeb0ae86ca95196f331f9b0333adb0aa95a7b";
@@ -148,7 +75,99 @@ $(document).ready(function() {
         $('#station5').html(displayPMI(stationsIDS.zlotyRog, 5));
 
         });
-   
+
+
+     };
+
+
+    // --------- code for powietrze.malopolska.pl api
+
+    const apiUrl = 'http://powietrze.malopolska.pl/_powietrzeapi/api/dane?act=danemiasta&ci_id=01';
+    const prefix = 'https://cors-anywhere.herokuapp.com/';
+    const finalUrl = prefix + apiUrl;
+
+    $.getJSON(finalUrl, function(results) {
+
+    // there are several ways the getJSON api download can fail:
+    // a) proxy can die  b) api can die  c) api and proxy can live but api can return null
+    // & possibly more
+    // added additional condition to check whether the random index returns true 
+    
+    if (results.dane.actual[5].details[0]) {    
+
+    
+        let defaultVal =  "n/a";
+
+        function checkForErr (id, position, property, tail) {
+            
+            if(property === 'station_name' && data[id][property] !== undefined) {
+                return data[id][property];
+
+            } else if(property === 'station_hour' && data[id][property] !== undefined) {
+                return data[id][property] + ":00";
+
+            } else if(position in data[id][property]){
+                
+                return data[id][property][position][tail];
+            } else {
+                return defaultVal;
+            }
+        }
+
+        var data = results.dane.actual;
+        
+    // object constructor for each field in which we're going to present the data
+
+        function StatusObject (id) {
+
+            this.fieldName = `#div${id}`;
+            this.name = checkForErr(id, null, 'station_name') ;
+            this.hour = checkForErr(id, null, 'station_hour');
+            this.status = checkForErr(id, 0, 'details', 'g_nazwa');
+            this.pm10 = checkForErr(id, 0, 'details', 'o_value');
+            this.pm25 = checkForErr(id, 1, 'details', 'o_value');
+            this.no2 = checkForErr(id, 2, 'details', 'o_value');
+            this.co = checkForErr(id, 3, 'details', 'o_value');
+    
+            this.getValues = function () {
+                return {
+                    "Nazwa stacji:"                 : this.name,
+                    "Godzina pomiaru:"              : this.hour,
+                    "Aktualna jakość powietrza:"    : this.status,
+                    "Poziom PM 10:"                 : this.pm10,
+                    "Poziom PM 2,5:"                : this.pm25,
+                    "Poziom NO2:"                   : this.no2,
+                    "Poziom CO:"                    : this.co
+                };
+
+            };
+        };
+
+        let Station0 = new StatusObject(0);
+        let Station1 = new StatusObject(1);
+        let Station2 = new StatusObject(2);
+        let Station3 = new StatusObject(3);
+        let Station4 = new StatusObject(4);
+        let Station5 = new StatusObject(5);
+
+console.log(Station0.getValues());
+console.log(Station1.getValues());
+console.log(Station2.getValues());
+console.log(Station3.getValues());
+console.log(Station4.getValues());
+console.log(Station5.getValues());
+
+    
+  
+    
+
+    } else {
+      useBackupAPI();
+    };
+
+}).fail(function() {  
+
+    useBackupAPI();
     
 });
 
