9bc988f3b7cfe0fb71050d2c113c222bd17952e5
p89 <pawel.prochal@gmail.com>
2017-03-07 22:54:17 +0100

    fine detailing

--- main.js
+++ main.js
@@ -6,11 +6,8 @@ $(document).ready(function() {
             case value <= 25:       
                 $(target).addClass('pm25');
                 break;
-            case value <= 50:
-                $(target).addClass('pm50');
-                break;
-            case value <= 75:
-                $(target).addClass('pm75');
+            case value < 60:
+                $(target).addClass('pm60');
                 break;
             case value <= 100:
                 $(target).addClass('pm100');
@@ -18,8 +15,11 @@ $(document).ready(function() {
             case value <= 150:
                 $(target).addClass('pm150');
                 break;
-            case value > 151:
-                $(target).addClass('pm151');
+            case value <= 200:
+                $(target).addClass('pm200');
+                break;
+            case value > 201:
+                $(target).addClass('pm201');
                 break;    
         }
     };
@@ -38,8 +38,6 @@ $(document).ready(function() {
         }
     }
 
-    let clouds = ['#kras','#bulw','#buja','#diet','#pias','#zlot'];
-
     function useBackupAPI () {
 
         // --------- code for api.waqi.info 
@@ -47,7 +45,8 @@ $(document).ready(function() {
         //alt token for Waqi bb892cfadf429d1f082713c403d0409f853d9559
         const apiWaqi = "https://api.waqi.info/map/bounds/?latlng=49.906700,20.172406,50.135416, 19.753533&token=116aeb0ae86ca95196f331f9b0333adb0aa95a7b";
         const backupApi = $.getJSON(apiWaqi, function(smogdata) {
-            
+        
+        console.log(smogdata);
         let stationsIDS = {
             zlotyRog:   8691,
             alejKras:   3402,
@@ -55,7 +54,8 @@ $(document).ready(function() {
             kurdwanow:  3407,
             piastow:    9039,
             bulwarowa:  3403
-        };   
+        };
+        let stations = [stationsIDS.alejKras, stationsIDS.bulwarowa, stationsIDS.kurdwanow, stationsIDS.dietla, stationsIDS.piastow, stationsIDS.zlotyRog];  
 
         let arr = smogdata.data.filter(function(index) { 
             for (num in stationsIDS) {
@@ -66,7 +66,7 @@ $(document).ready(function() {
         });
 
         function displayPMI (stationID, index) { 
-
+            let value = 0;
             for (let i = 0; i < arr.length; i++) {
 
                 let divNr = `#div${index}`; 
@@ -77,21 +77,19 @@ $(document).ready(function() {
                     }
                 }
             }
-            $(`#cloudInfo${index}`).html('<div class="backupCloud">PM10: ' + value + '</div>');
+            $(`#cloudInfo${index}`).html('<div class="backupCloud">AQI: ' + value + '</div>');
         };
 
-        displayPMI(stationsIDS.alejKras, 0);
-        displayPMI(stationsIDS.bulwarowa, 1);
-        displayPMI(stationsIDS.kurdwanow, 2);
-        displayPMI(stationsIDS.dietla, 3);
-        displayPMI(stationsIDS.piastow, 4);
-        displayPMI(stationsIDS.zlotyRog, 5);
-
+        for (station in stations) {
+            displayPMI(stations[station], station);
+        }
+        
         cloudHandlerStart();
 
         });
      };
 
+    const clouds = ['#kras','#bulw','#buja','#diet','#pias','#zlot']; 
 
     // --------- code for powietrze.malopolska.pl api
 
@@ -103,13 +101,12 @@ $(document).ready(function() {
 
     // there are several ways the getJSON api download can fail:
     // a) proxy can die  b) api can die  c) api and proxy can live but api can return null
-    // & possibly more
+    // & more
     // added additional condition to check whether the random index returns true 
     
     if (results.dane.actual[5].details[0]) {    
 
-    
-        let defaultVal =  "n/a";
+        const defaultVal =  "n/a";
 
         function checkForErr (id, position, property, tail) {
             
@@ -124,7 +121,7 @@ $(document).ready(function() {
             }
         }
 
-        var data = results.dane.actual;
+        let data = results.dane.actual;
         
     // object constructor for each field in which we're going to present the data
 
@@ -159,7 +156,6 @@ $(document).ready(function() {
         let Station5 = new StatusObject(5);
         let stations = [Station0, Station1, Station2, Station3, Station4, Station5];
         
-
         function displayPMImain (index, tArray) { 
             $(`#station${index}`).html(tArray[index].name);
             buttonColoring(tArray[index].pm10, `#div${index}`);
@@ -183,7 +179,5 @@ $(document).ready(function() {
     } else {
       useBackupAPI();
     };
-}).fail(function() {  
-    useBackupAPI();
-    });
+}).fail(() => useBackupAPI()) 
 });
